133e230aa9297ad4cca17cfd724fc7ec77910424
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-06-23 23:50:13 -0300

    criando a logica do sistema de pontuacao do jogo

---
+++ Assets/scriptControlador.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptControlador : MonoBehaviour
+{
+    public static int placar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        placar = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
